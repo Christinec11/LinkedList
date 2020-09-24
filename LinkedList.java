e53edc2c745ec69f6ac7c69f99f0213f2680bdfa
Christine <Othgirl23@Christines-MacBook-Air.local>
2020-09-23 23:36:30 -0400

    start

---
+++ LinkedList.java
@@ -0,0 +1,100 @@
+package LinkedList;
+
+public class LinkedList {
+    private Node head;
+    private int size;
+
+    public LinkedList() {
+        head = new Node();
+        size = 0;
+    }
+
+    public void add(Object item) {
+        Node curr = head;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        curr.next = new Node(item);
+        size++;
+    }
+
+    public void add(int index, Object item) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Good luck, private Ryan!");
+        }
+        Node curr = head;
+        int i = 0;
+        while (i != index) {
+            curr = curr.next;
+            i++;
+        }
+        curr.next = new Node(item, curr.next);
+        size++;
+    }
+
+    public Object get(int index) {
+        if (index < 0) {
+            index = size + index;
+        }
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Good luck, private Ryan!");
+        }
+
+        Node curr = head;
+        int i = 0;
+        while (i - 1 != index) {
+            curr = curr.next;
+            i++;
+        }
+        return curr.item;
+    }
+
+    public boolean contains(Object item) {
+        Node curr = head.next;
+        while (curr != null) {
+            if (curr.item.equals(item)) {
+                return true;
+            }
+            curr = curr.next;
+        }
+        return false;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public String toString() {
+        String out = "";
+        for (Node curr = head.next; curr != null; curr = curr.next) {
+            out += curr.item + " ";
+        }
+        return out;
+    }
+        /** 
+* Removes the first matched object and
+* returns the position of the removed object
+* from the LinkedList.
+* 
+* @param item represents the item the user wants to remove
+* @return     position of the removed item
+*/
+int remove(Object item) 
+{
+
+}
+
+/**
+* Removes the object given the position and
+* returns the removed object from the LinkedList.
+* 
+* @param position represents the position of the object
+*                 to be removed
+* @return         the item that was removed
+*/
+Object remove(int position) 
+{
+
+}
+
+}
